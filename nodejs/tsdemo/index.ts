6285db7861905a9ae39e90405caa5f76a831a55b
tangquanlie <tangquanlie@palmax.cn>
2023-04-23 09:35:50 +0800

    nodejs

--- nodejs/tsdemo/index.ts
+++ nodejs/tsdemo/index.ts
@@ -1,2 +1,21 @@
 let a: number = 9999
 console.log(a)
+
+
+let storage: Record<string, number> = {}
+const key: string = "abc"
+function add() {
+	if (!storage[key]) {
+		storage[key] = 1
+	} else {
+		storage[key] = storage[key] + 1
+	}
+}
+
+console.log("init ", storage[key])
+add()
+console.log("after add ", storage[key])
+add()
+console.log("after add ", storage[key])
+
+
--- web/nestjs.md
+++ web/nestjs.md
@@ -7,9 +7,25 @@ https://docs.nestjs.cn/
 
 npm i -g @nestjs/cli
 
+npm install npm@7.1.0 -g
+
+sudo npm uninstall npm -g
+
 nest new project-name
 
 nest g controller cats
 
 nest g resource name
+
+
+
+
+
+https://docs.nestjs.com/
+
+$ git clone https://github.com/nestjs/typescript-starter.git project
+$ cd project
+$ npm install
+$ npm run start
+
 
